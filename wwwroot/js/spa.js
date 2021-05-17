e5d47a75b354dc72ee11467a527d4b4b78ede38c
Jakob Russell <jakob.russell@students.plymouth.ac.uk>
2021-05-17 16:39:12 +0100

    Add project files.

---
+++ wwwroot/js/spa.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $('#scan').click(function () {
+        $('#page-content-wrapper').load('QR.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#htmlpage').click(function () {
+        $('#page-content-wrapper').load('htmlpage.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#amend').click(function () {
+        $('#page-content-wrapper').load('Amend.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#book').click(function () {
+        $('#page-content-wrapper').load('Booking.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#staff').click(function () {
+        $('#page-content-wrapper').load('Staff.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#bookp2').click(function () {
+        $('#page-content-wrapper').load('Booking2.html');
+    })
+});
